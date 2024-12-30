e68c0b51dfef6a5f219fee5e8dce98087b93e82c
susangautam <off.susangautam@gmail.com>
2024-12-30 12:07:40 +0545

    messsage

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/portfolio/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
     },
+    
   },
 })
