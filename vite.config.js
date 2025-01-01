50c26b9c05f7baeb57cc865e1827fce4ef5954f3
susangautam <off.susangautam@gmail.com>
2025-01-01 20:06:03 +0545

    push

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/susan-web/",
+ 
   plugins: [react()],
+  base:"/susan-web/",
   resolve: {
     alias: {
       "@": "/src",
